b37a92b4161629ef8e76de000d8459a17f7eea21
oleghenkel <oleg.henkel75@gmail.com>
2023-05-06 18:26:46 +0500

    Базовая сложность

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,41 @@
 public class Employee {
+private String fullName;
+private int department;
+private double salary;
+private  int id;
+private static int counter = 0;
+
+//Getter
+    public String getFullName(){
+        return this.fullName;}
+    public int getDepartment(){
+        return this.department;}
+    public double getSalary(){
+        return this.salary;}
+    public int getId(){
+        return this.id;}
+
+    //Setter
+
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    //Constructor
+    public Employee(String fullName, int department, double salary){
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        this.id = ++counter;
+    }
+    @Override
+    public String toString(){
+        return "Id: " + id + " Ф.И.О.: " + fullName + " Department: " + department + " Salary: " + salary;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,85 @@
 public class Main {
+    public static final Employee [] employees = {
+            new Employee("Аверин Юрий Алексеевич", 1, 50000),
+            new Employee("Варламов Владимир Иванович", 2, 60000),
+            new Employee("Веденеев Илья Николаевич", 1, 55000),
+            new Employee("Давыдов Геннадий Анатольевич", 3, 70000),
+            new Employee("Долгов Виталий Игоревич", 4, 80000),
+            new Employee("Иванов Григорий Алексеевич", 2, 65000),
+            new Employee("Карташов Владислав Сергеевич", 5, 90000),
+            new Employee("Макаров Константин Валентинович", 4, 75000),
+            new Employee("Панфилов Никита Максимович", 3, 65000),
+            new Employee("Тетерин Павел Викторович", 5, 95000)
+
+    };
     public static void main(String[] args) {
 
+        printAllEmployees();
+        System.out.println("---------------------------------");
+
+        System.out.println(calculateTotalSalary());
+        System.out.println("---------------------------------");
+
+        System.out.println(getEmployeeWithMinSalary());
+        System.out.println("---------------------------------");
+
+        System.out.println(getEmployeeWithMaxSalary());
+        System.out.println("---------------------------------");
+
+        System.out.println(calculateAverageSalary());
+        System.out.println("---------------------------------");
+
+        printAllNames();
+
+
+
+    }
+
+
+    public static void printAllEmployees() {
+        for (Employee emp : employees) {
+            System.out.println(emp.toString());
+        }
+    }
+
+    public static double calculateTotalSalary() {
+        double totalSalary = 0;
+        for (Employee emp: employees) {
+            totalSalary += emp.getSalary();
+        }
+        return totalSalary;
+    }
+
+    public static Employee getEmployeeWithMinSalary() {
+        Employee minEmployee = employees[0];
+        for (Employee employee: employees) {
+            if (employee.getSalary() < minEmployee.getSalary()) {
+                minEmployee = employee;
+            }
+        }
+        return minEmployee;
+
+    }
+
+
+    public static Employee getEmployeeWithMaxSalary() {
+        Employee maxEmployee = employees[0];
+        for (int i = 1; i < employees.length; i++) {
+            if (employees[i].getSalary() > maxEmployee.getSalary()) {
+                maxEmployee = employees[i];
+            }
+        }
+        return maxEmployee;
+    }
+
+    public static double calculateAverageSalary() {
+        double totalSalary = calculateTotalSalary();
+        return totalSalary / employees.length;
+    }
+
+    public static void printAllNames() {
+        for (Employee emp : employees) {
+            System.out.println(emp.getFullName());
+        }
     }
 }
