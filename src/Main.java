4186982431168d406b91da728aa576a945ce1a3c
oleghenkel <oleg.henkel75@gmail.com>
2023-05-06 19:16:21 +0500

    Повышенная сложность

--- src/Main.java
+++ src/Main.java
@@ -14,22 +14,49 @@ public class Main {
     };
     public static void main(String[] args) {
 
+       // printAllEmployees();
+        //System.out.println("---------------------------------");
+
+        //System.out.println(calculateTotalSalary());
+        //System.out.println("---------------------------------");
+
+        //System.out.println(getEmployeeWithMinSalary());
+        //System.out.println("---------------------------------");
+
+        //System.out.println(getEmployeeWithMaxSalary());
+        //System.out.println("---------------------------------");
+
+        //System.out.println(calculateAverageSalary());
+        //System.out.println("---------------------------------");
+
+        //printAllNames();
+        increaseSalary(20);
         printAllEmployees();
         System.out.println("---------------------------------");
 
-        System.out.println(calculateTotalSalary());
+        System.out.println(findEmployeeWithMaxSalaryByDepartment(5));
         System.out.println("---------------------------------");
 
-        System.out.println(getEmployeeWithMinSalary());
+        System.out.println(findEmployeeWithMinSalaryByDepartment(4));
         System.out.println("---------------------------------");
 
-        System.out.println(getEmployeeWithMaxSalary());
+        System.out.println(calculateSalaryCostsByDepartment(3));
         System.out.println("---------------------------------");
 
-        System.out.println(calculateAverageSalary());
+        System.out.println(calculateAverageSalaryByDepartment(2));
         System.out.println("---------------------------------");
 
-        printAllNames();
+        increaseSalaryByPercentForDepartment(2, 20);
+        printEmployeesByDepartment(2);
+        System.out.println("---------------------------------");
+
+        printEmployeesWithSalaryLessThan(70000);
+        System.out.println("---------------------------------");
+
+        printEmployeesWithSalaryGreaterThanOrEqualTo(70000);
+        System.out.println("---------------------------------");
+
+
 
 
 
@@ -80,6 +107,95 @@ public class Main {
     public static void printAllNames() {
         for (Employee emp : employees) {
             System.out.println(emp.getFullName());
+        }
+    }
+    public static void increaseSalary(double percent) {
+        for (Employee employee : employees) {
+            double currentSalary = employee.getSalary();
+            employee.setSalary(currentSalary * (1 + percent / 100));
+        }
+    }
+
+    public static Employee findEmployeeWithMinSalaryByDepartment(int department) {
+        Employee minSalaryEmployee = null;
+        for (Employee employee : employees) {
+            if (employee.getDepartment() == department) {
+                if (minSalaryEmployee == null || employee.getSalary() < minSalaryEmployee.getSalary()) {
+                    minSalaryEmployee = employee;
+                }
+            }
+        }
+        return minSalaryEmployee;
+    }
+
+    public static Employee findEmployeeWithMaxSalaryByDepartment(int department) {
+        Employee maxSalaryEmployee = null;
+        for (Employee employee : employees) {
+            if (employee.getDepartment() == department) {
+                if (maxSalaryEmployee == null || employee.getSalary() > maxSalaryEmployee.getSalary()) {
+                    maxSalaryEmployee = employee;
+                }
+            }
+        }
+        return maxSalaryEmployee;
+    }
+
+    public static double calculateSalaryCostsByDepartment(int department) {
+        double salaryCosts = 0;
+        for (Employee employee : employees) {
+            if (employee.getDepartment() == department) {
+                salaryCosts += employee.getSalary();
+            }
+        }
+        return salaryCosts;
+    }
+
+    public static double calculateAverageSalaryByDepartment(int department) {
+        double sumSalary = 0;
+        int numEmployees = 0;
+        for (Employee employee : employees) {
+            if (employee.getDepartment() == department) {
+                sumSalary += employee.getSalary();
+                numEmployees++;
+            }
+        }
+        if (numEmployees == 0) {
+            return 0;
+        } else {
+            return sumSalary / numEmployees;
+        }
+    }
+
+    public static void increaseSalaryByPercentForDepartment(int department, double percent) {
+        for (Employee employee : employees) {
+            if (employee.getDepartment() == department) {
+                double salary = employee.getSalary();
+                employee.setSalary(salary * (1 + percent / 100));
+            }
+        }
+    }
+
+    public static void printEmployeesByDepartment(int department) {
+        for (Employee employee : employees) {
+            if (employee.getDepartment() == department) {
+                System.out.println(employee.getFullName());
+            }
+        }
+    }
+
+    public static void printEmployeesWithSalaryLessThan(double salary) {
+        for (Employee employee : employees) {
+            if (employee.getSalary() < salary) {
+                System.out.println("id: " + employee.getId() + ", Ф.И.О.: " + employee.getFullName() + ", зарплата: " + employee.getSalary());
+            }
+        }
+    }
+
+    public static void printEmployeesWithSalaryGreaterThanOrEqualTo(double salary) {
+        for (Employee employee : employees) {
+            if (employee.getSalary() >= salary) {
+                System.out.println("id: " + employee.getId() + ", Ф.И.О.: " + employee.getFullName() + ", зарплата: " + employee.getSalary());
+            }
         }
     }
 }
